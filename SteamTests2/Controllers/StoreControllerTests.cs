24397113a631e1a7b25eb3984318810e01d944c1
Stijn <stijn.vanderlinden@student.fontys.nl>
2017-01-07 17:53:25 +0100

    Unit tests
    Unit tests aanmaken.

---
+++ SteamTests2/Controllers/StoreControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using Steam.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steam.Controllers.Tests
+{
+    [TestClass()]
+    public class StoreControllerTests
+    {
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            var controller = new StoreController();
+            var result = controller.Details(2) as ViewResult;
+            Assert.AreEqual("Details", result.ViewName);
+        }
+    }
+}
---
+++ SteamTests3/Controllers/StoreControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Steam.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Steam.Controllers.Tests
+{
+    [TestClass()]
+    public class StoreControllerTests
+    {
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            var controller = new StoreController();
+            var result = controller.Details(1) as ViewResult;
+            Assert.AreEqual("", result.ViewName);
+        }
+    }
+}
